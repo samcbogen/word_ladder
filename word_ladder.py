55b1d57f99868d5626f38da357d7c5d196791c42
samcbogen <Samuel.Bogen.23@cmc.edu>
2021-02-23 20:36:00 -0800

    minor fix

--- word_ladder.py
+++ word_ladder.py
@@ -1,37 +1,8 @@
-#!/bin/python3
 from collections import deque
 import copy
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
-    '''
-    Returns a list satisfying the following properties:
-
-    1. the first element is `start_word`
-    2. the last element is `end_word`
-    3. elements at index i and i+1 are `_adjacent`
-    4. all elements are entries in the `dictionary_file` file
-
-    For example, running the command
-    ```
-    word_ladder('stone','money')
-    ```
-    may give the output
-    ```
-    ['stone', 'shone', 'phone', 'phony', 'peony',
-    i'penny', 'benny', 'bonny', 'boney', 'money']
-    ```
-    but the possible outputs are not unique,
-    so you may also get the output
-    ```
-    ['stone', 'shone', 'shote', 'shots',
-    'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
-    ```
-    (We cannot use doctests here because the outputs are not unique.)
-
-    Whenever it is impossible to generate a word ladder between the two words,
-    the function returns `None`.
-    '''
     with open(dictionary_file) as f:
         new_dictionary = f.readlines()
 
